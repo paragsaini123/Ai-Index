024d2fecf6601a9a8bc5fe465c4644341101438b
paragsaini123 <parag.saini@genpact.com>
2024-10-11 19:51:05 +0530

    Added okta sso implementation from personal dev account

---
+++ Auth.js
@@ -0,0 +1,186 @@
+import React, { Component, Fragment } from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {
+  createConfig,
+  signIn,
+  signOut,
+  isAuthenticated,
+  getUser,
+  getUserFromIdToken,
+  EventEmitter,
+} from '@okta/okta-react-native';
+import configFile from './auth.config';
+
+export default class Auth extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      context: null,
+    };
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+  }
+
+  async componentDidMount() {
+    let that = this;
+    EventEmitter.addListener('signInSuccess', function (e) {
+      that.setState({ authenticated: true });
+      that.setContext('Logged in!');
+    });
+    EventEmitter.addListener('signOutSuccess', function (e) {
+      that.setState({ authenticated: false });
+      that.setContext('Logged out!');
+    });
+    EventEmitter.addListener('onError', function (e) {
+      console.warn(e);
+      that.setContext(e.error_message);
+    });
+    EventEmitter.addListener('onCancelled', function (e) {
+      console.warn(e);
+    });
+    await createConfig({
+      clientId: configFile.oidc.clientId,
+      redirectUri: configFile.oidc.redirectUri,
+      endSessionRedirectUri: configFile.oidc.endSessionRedirectUri,
+      discoveryUri: configFile.oidc.discoveryUri,
+      scopes: configFile.oidc.scopes,
+      requireHardwareBackedKeyStore:
+        configFile.oidc.requireHardwareBackedKeyStore,
+    });
+    await this.checkAuthentication();
+  }
+
+  componentWillUnmount() {
+    EventEmitter.removeAllListeners('signInSuccess');
+    EventEmitter.removeAllListeners('signOutSuccess');
+    EventEmitter.removeAllListeners('onError');
+    EventEmitter.removeAllListeners('onCancelled');
+  }
+
+  async componentDidUpdate() {
+    await this.checkAuthentication();
+  }
+
+  async checkAuthentication() {
+    const result = await isAuthenticated();
+    if (result?.authenticated !== this.state.authenticated) {
+      this.setState({ authenticated: result?.authenticated });
+    }
+  }
+
+  async login() {
+    await signIn();
+  }
+
+  async logout() {
+    await signOut();
+  }
+
+  async getUserIdToken() {
+    let user = await getUserFromIdToken();
+    this.setContext(JSON.stringify(user, null, 2));
+  }
+
+  async getMyUser() {
+    let user = await getUser();
+    this.setContext(JSON.stringify(user, null, 2));
+  }
+
+  setContext = message => {
+    this.setState({
+      context: message,
+    });
+  };
+
+  renderButtons() {
+    if (this.state.authenticated) {
+      return (
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              onPress={async () => {
+                await this.getUserIdToken();
+              }}
+              title="Get User From Id Token"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              onPress={async () => {
+                await this.getMyUser();
+              }}
+              title="Get User From Request"
+            />
+          </View>
+        </View>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              {this.state.authenticated ? (
+                <Button
+                  style={styles.button}
+                  testID="logoutButton"
+                  onPress={async () => {
+                    await this.logout();
+                  }}
+                  title="Logout"
+                />
+              ) : (
+                <Button
+                  style={styles.button}
+                  testID="loginButton"
+                  onPress={async () => {
+                    await this.login();
+                  }}
+                  title="Login"
+                />
+              )}
+            </View>
+          </View>
+          {this.renderButtons()}
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.infoBox}>
+            <Text>{this.state.context}</Text>
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: 300,
+    height: 40,
+    marginTop: 10,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  infoBox: {
+    backgroundColor: 'lightskyblue',
+    borderRadius: 5,
+  },
+});
---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oakazvqa4DCrHl3e5d7',
+    redirectUri: 'com.okta.dev-88123533:/callback',
+    endSessionRedirectUri: 'com.okta.dev-88123533:/callback',
+    discoveryUri: 'https://dev-88123533.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
